7553110d537b81d9b5f889e37e54bb3e466e852e
Kai-Wei Tsou <cmpss89300@gmail.com>
2019-03-07 16:15:58 +0100

    Update models.py

--- models.py
+++ models.py
@@ -58,3 +58,57 @@ class UNet(object):
         self.loss = loss(y, t)
         self.training = tf.train.AdamOptimizer(self.LR).minimize(self.loss)
 
+
+class FCN(object):
+    def __init__(self, x, t, LR, input_shape, output_shape, model_name='U-Net'):
+        # optimization setting
+        self.LR = LR
+        
+        # naming setting
+        self.model_name = model_name
+        
+        # model setting
+        self.input_shape = input_shape
+        self.output_shape = output_shape
+        with tf.variable_scope(self.model_name):
+            self.x = x
+            self.t = t
+            self.y = self._forward_pass(self.x)
+            #self.x = tf.placeholder(dtype=tf.float32, shape=self.input_shape, name='input')
+            #self.t = tf.placeholder(dtype=tf.float32, shape=self.output_shape, name='output')
+            #self.y = self._forward_pass(self.x)
+
+    def _forward_pass(self, x):
+        # Encoder
+        h1 = conv_layer(x, filter_shape=[3, 3, 3, 64], name='L1') # (90, 422, 64)
+        h2 = conv_layer(h1, filter_shape=[3, 3, 64, 64], name='L2')
+        h3 = pooling_layer(h2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], name='L3') # (45, 211, 64)
+        h4 = conv_layer(h3, filter_shape=[3, 3, 64, 256], name='L4')
+        h5 = conv_layer(h4, filter_shape=[3, 3, 256, 256], name='L5')
+        h6 = pooling_layer(h5, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], name='L6') # (23, 106, 128)
+        h7 = conv_layer(h6, filter_shape=[3, 3, 256, 512], name='L7')
+        h8 = conv_layer(h7, filter_shape=[3, 3, 512, 512], name='L8')
+        h9 = pooling_layer(h8, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], name='L9') # (12, 53, 256)
+        h10 = conv_layer(h9, filter_shape=[3, 3, 512, 1024], name='L10')
+        h11 = conv_layer(h10, filter_shape=[3, 3, 1024, 1024], name='L11')
+        # Decoder
+        h12 = deconv_layer(h11, filter_shape=[3, 3, 1024, 1024], strides=[1, 2, 2, 1], output_shape=[-1, 24, 106, 1024], name='L12')
+        h13 = conv_layer(h12, filter_shape=[3, 3, 1024, 512], name='L13')
+        h14 = conv_layer(h13, filter_shape=[3, 3, 512, 512], name='L14')
+        h15 = deconv_layer(h14, filter_shape=[3, 3, 512, 512], strides=[1, 2, 2, 1], output_shape=[-1, 46, 212, 512], name='L15')
+        h16 = conv_layer(h15, filter_shape=[3, 3, 512, 256], name='L16')
+        h17 = conv_layer(h16, filter_shape=[3, 3, 256, 256], name='L17')
+        h18 = deconv_layer(h17, filter_shape=[3, 3, 256, 256], strides=[1, 2, 2, 1], output_shape=[-1, 90, 422, 256], name='L18')
+        h19 = conv_layer(h18, filter_shape=[3, 3, 256, 64], name='L19')
+        h20 = conv_layer(h19, filter_shape=[3, 3, 64, 64], name='L20')
+        h21 = conv_layer(h20, filter_shape=[1, 1, 64, self.output_shape[3]], name='L21', non_linear=None)
+        return h21
+    
+    def optimize(self, loss):
+        shape = tf.shape(self.y) # [batch_size, height, width, class]
+        y = tf.reshape(self.y, [shape[0]*shape[1]*shape[2], shape[3]])
+        t = tf.reshape(self.t, [shape[0]*shape[1]*shape[2], shape[3]])
+        
+        self.loss = loss(y, t)
+        self.training = tf.train.AdamOptimizer(self.LR).minimize(self.loss)
+
