9f7989fded596dd83f8a90e52a79fd39d8395848
Ayush <ayush@pop-os.localdomain>
2019-07-06 21:24:11 +0200

    added files

---
+++ Mask_RCNN/Test_VIC_Project.py
@@ -0,0 +1,214 @@
+#Code to Test the Mask_RCNN on WAD Test and Validation Dataset
+
+import os
+import sys
+import json
+import numpy as np
+import time
+from PIL import Image, ImageDraw
+
+ROOT_DIR = './'
+assert os.path.exists(ROOT_DIR), 'ROOT_DIR does not exist.'
+
+# Import mrcnn libraries
+sys.path.append(ROOT_DIR) 
+from mrcnn.config import Config
+import mrcnn.utils as utils
+from mrcnn import visualize
+import mrcnn.model as modellib
+
+# Directory to save logs and trained model
+MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+
+# Local path to trained weights file
+COCO_MODEL_PATH = os.path.join(ROOT_DIR,"Pretrained_Model","mask_rcnn_coco.h5")
+
+# ## Configuration
+# Define configurations for training on the Kaggle WAD Dataset
+
+class WADConfig(Config):
+    """Configuration for training on the cigarette butts dataset.
+    Derives from the base Config class and overrides values specific
+    to the cigarette butts dataset.
+    """
+    # Give the configuration a recognizable name
+    NAME = "WAD"
+
+    # Train on 1 GPU and 1 image per GPU. Batch size is 1 (GPUs * images/GPU).
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 5
+
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 7  # background + 1 (7 classes in our dataset)
+
+    # All of our training images are 512x512
+    IMAGE_MIN_DIM = 640
+    IMAGE_MAX_DIM = 832
+
+    # You can experiment with this number to see if it improves training
+    STEPS_PER_EPOCH = 10
+
+    # This is how often validation is run. If you are using too much hard drive space
+    # on saved models (in the MODEL_DIR), try making this value larger.
+    VALIDATION_STEPS = 5
+    
+    # Can also use resnet101
+    BACKBONE = 'resnet50'
+
+    # Set Region Proposal Anchor Scales
+    RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)
+    TRAIN_ROIS_PER_IMAGE = 32
+    MAX_GT_INSTANCES = 50 
+    POST_NMS_ROIS_INFERENCE = 500 
+    POST_NMS_ROIS_TRAINING = 1000 
+    
+config = WADConfig()
+config.display()
+
+
+# # Define the dataset
+# Generic COCO Like Dataset Loader Class
+
+class CocoLikeDataset(utils.Dataset):
+    """ Generates a COCO-like dataset, i.e. an image dataset annotated in the style of the COCO dataset.
+        See http://cocodataset.org/#home for more information.
+    """
+    def load_data(self, annotation_json, images_dir):
+        """ Load the coco-like dataset from json
+        Args:
+            annotation_json: The path to the coco annotations json file
+            images_dir: The directory holding the images referred to by the json file
+        """
+        # Load json from file
+        json_file = open(annotation_json)
+        coco_json = json.load(json_file)
+        json_file.close()
+        
+        # Add the class names using the base method from utils.Dataset
+        source_name = "coco_like"
+        for category in coco_json['categories']:
+            class_id = category['id']
+            class_name = category['name']
+            if class_id < 1:
+                print('Error: Class id for "{}" cannot be less than one. (0 is reserved for the background)'.format(class_name))
+                return
+            
+            self.add_class(source_name, class_id, class_name)
+        
+        # Get all annotations
+        annotations = {}
+        for annotation in coco_json['annotations']:
+            image_id = annotation['image_id']
+            if image_id not in annotations:
+                annotations[image_id] = []
+            annotations[image_id].append(annotation)
+        
+        # Get all images and add them to the dataset
+        seen_images = {}
+        for image in coco_json['images']:
+            image_id = image['id']
+            if image_id in seen_images:
+                print("Warning: Skipping duplicate image id: {}".format(image))
+            else:
+                seen_images[image_id] = image
+                try:
+                    image_file_name = image['file_name']
+                    image_width = image['width']
+                    image_height = image['height']
+                except KeyError as key:
+                    print("Warning: Skipping image (id: {}) with missing key: {}".format(image_id, key))
+                
+                image_path = os.path.abspath(os.path.join(images_dir, image_file_name))
+                image_annotations = annotations[image_id]
+                
+                # Add the image using the base method from utils.Dataset
+                self.add_image(
+                    source=source_name,
+                    image_id=image_id,
+                    path=image_path,
+                    width=image_width,
+                    height=image_height,
+                    annotations=image_annotations
+                )
+                
+    def load_mask(self, image_id):
+        """ Load instance masks for the given image.
+        MaskRCNN expects masks in the form of a bitmap [height, width, instances].
+        Args:
+            image_id: The id of the image to load masks for
+        Returns:
+            masks: A bool array of shape [height, width, instance count] with
+                one mask per instance.
+            class_ids: a 1D array of class IDs of the instance masks.
+        """
+        image_info = self.image_info[image_id]
+        annotations = image_info['annotations']
+        instance_masks = []
+        class_ids = []
+        
+        for annotation in annotations:
+            class_id = annotation['category_id']
+            mask = Image.new('1', (image_info['width'], image_info['height']))
+            mask_draw = ImageDraw.ImageDraw(mask, '1')
+            for segmentation in annotation['segmentation']:
+                mask_draw.polygon(segmentation, fill=1)
+                bool_array = np.array(mask) > 0
+                instance_masks.append(bool_array)
+                class_ids.append(class_id)
+
+        mask = np.dstack(instance_masks)
+        class_ids = np.array(class_ids, dtype=np.int32)
+        
+        return mask, class_ids
+
+#-------------------------------------------------------------------------------------------------
+
+# # Prepare to run Inference
+# Create a new InferenceConfig, then use it to create a new model.
+
+
+class InferenceConfig(WADConfig):
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+    IMAGE_MIN_DIM = 640
+    IMAGE_MAX_DIM = 832
+    DETECTION_MIN_CONFIDENCE = 0.7
+    
+
+inference_config = InferenceConfig()
+
+
+# Recreate the model in inference mode
+model = modellib.MaskRCNN(mode="inference", 
+                          config=inference_config,
+                          model_dir=MODEL_DIR)
+
+
+# Get path to saved weights
+# Either set a specific path or find last trained weights
+model_path = os.path.join(ROOT_DIR, ".h5 file name here")
+model_path = model.find_last()
+
+# Load trained weights (fill in path to trained weights here)
+assert model_path != "", "Provide path to trained weights"
+print("Loading weights from ", model_path)
+model.load_weights(model_path, by_name=True)
+
+
+# # Run Inference
+
+import skimage
+real_test_dir = '/home/ayush/Instance_Segmentation/all/Sample_Dataset/sample_test'
+image_paths = []
+for filename in os.listdir(real_test_dir):
+    if os.path.splitext(filename)[1].lower() in ['.png', '.jpg', '.jpeg']:
+        image_paths.append(os.path.join(real_test_dir, filename))
+
+for image_path in image_paths:
+    img = skimage.io.imread(image_path)
+    img_arr = np.array(img)
+    results = model.detect([img_arr], verbose=0)
+    r = results[0]
+#   visualize.display_instances(img, r['rois'], r['masks'], r['class_ids'], dataset_val.class_names, r['scores'], figsize=(5,5))
+
+
---
+++ Mask_RCNN/create_dataset.py
@@ -0,0 +1,277 @@
+from PIL import Image 
+from skimage import measure
+from shapely.geometry import Polygon, MultiPolygon
+from image_processing import resize_label, resize_image
+
+import os
+import numpy as np
+import tqdm
+import datetime
+import json
+import pdb
+
+
+id2classid = {33:1,
+              34:2,
+              35:3,
+              36:4,
+              38:5,
+              39:6,
+              40:7,}
+
+category2id = {'car':          33,
+               'motorbicycle': 34, 
+               'bicycle':      35,
+               'person':       36, 
+               'truck':        38,
+               'bus':          39,
+               'tricycle':     40,}
+id2category = {}
+for cat, _id in category2id.items():
+    id2category[_id] = cat
+
+def create_coco(img_path, label_path, destination, crop_size=None, down_scale=None):
+    
+    label_path.sort()
+    img_path.sort()
+    
+    # create images list
+    print('Creating image list...')
+    image_list = create_image_list(img_path, crop_size, down_scale)
+    
+    # create annotation list
+    print('Creating object2color dictionary...')
+    object2color, class_instance = create_object2color(label_path)
+    category_ids = create_color2category(class_instance, object2color)
+    
+    is_crowd = 0
+
+    # These ids will be automatically increased as we go
+    annotation_id = 0
+
+    # Create the annotations
+    print('Creating annotation list...')
+    annotations = []
+    for img_id, path in tqdm.tqdm(enumerate(label_path)):
+        label = Image.open(path)
+        label = np.array(label)
+        
+        if (crop_size is not None) and (down_scale is not None):
+            label = resize_label(label, crop_size=crop_size, down_scale=down_scale)
+        
+        mask = do_mask_image(label, class_instance[img_id], object2color)
+        mask = Image.fromarray(np.uint8(mask))
+        sub_masks = create_sub_masks(mask)
+        for color, sub_mask in sub_masks.items():
+            category_id = id2classid[category_ids[img_id][color]]
+            #annotation = create_sub_mask_annotation(sub_mask, img_id, category_id, annotation_id, is_crowd)
+            try:
+                annotation = create_sub_mask_annotation(sub_mask, img_id, category_id, annotation_id, is_crowd)
+            except:
+                print('Image ID ', img_id, 'could not find the bounding boxs')
+                continue
+            annotations.append(annotation)
+            annotation_id += 1
+
+    # Delete images withoud annotation
+    image_list = clean_image_list(image_list, annotations)
+    
+    # Making json
+    data = {}
+    data['info'] = {}
+    data['licenses'] = []
+    data['images'] = image_list
+    data['annotations'] = annotations
+    data['categories'] = [
+        {"supercategory": "vehicle", "id": 1,"name": "car"},
+        {"supercategory": "vehicle", "id": 2,"name": "motorbicycle"},
+        {"supercategory": "vehicle", "id": 3,"name": "bicycle"},
+        {"supercategory": "vehicle", "id": 5,"name": "truck"},
+        {"supercategory": "vehicle", "id": 6,"name": "bus"},
+        {"supercategory": "vehicle", "id": 7,"name": "tricycle"},
+
+        {"supercategory": "person", "id": 4,"name": "person"},
+    ]
+    with open(destination, 'w') as f:
+        json.dump(data, f, indent=4)
+
+def create_image_list(img_path, crop_size=None, down_scale=None):
+    img = Image.open(img_path[0])
+    h = img.height
+    w = img.width
+    
+    if (crop_size is not None) and (down_scale is not None):
+        img = np.array(img)
+        img = resize_image(img, crop_size=crop_size, down_scale=down_scale)
+        h, w, _ = img.shape
+    
+    image_list = []
+    for img_id, path in enumerate(img_path):
+        image_list += [{
+            'id': img_id,
+            'license': 0,
+            'coco_url': 'https://www.google.com/',
+            'flickr_url': 'https://www.google.com/',
+            'width': w,
+            'height': h,
+            'file_name': path.split('/')[-1],
+            'date_captured': datetime.datetime.now().replace(microsecond=0).isoformat(' '),
+        }]
+    return image_list
+
+
+#def create_object2color(label_path, id2category):
+def create_object2color(label_path):
+    
+    class_instance = {}
+    class_summary = {}
+    for _id in id2category.keys():
+        class_summary[_id] = set()
+    
+    for img_id, path in tqdm.tqdm(enumerate(label_path)):
+        label = Image.open(path)
+        label = np.array(label)
+        label_class = np.ndarray.astype(label/1000, np.int32)
+        label_instance = np.mod(label, 1000)
+        
+        class_instance[img_id] = []
+        for class_id in id2category.keys():
+            i, j = np.where(label_class==class_id)
+            instance_set = set(label_instance[i, j])
+            
+            class_summary[class_id] = class_summary[class_id].union(instance_set)
+            for instance_id in instance_set:
+                class_instance[img_id] += [(class_id, instance_id)]
+            
+    object2color = {}
+    color_set = set()
+    def generate_color(color_set):
+        while True:
+            r, g, b = np.random.randint(256, size=3)
+            if not (r, g, b) in color_set:
+                break
+        return (r, g, b)
+    
+    for class_id, instances_id in class_summary.items():
+        for instance_id in instances_id:
+            r, g, b = generate_color(color_set)
+            object2color[(id2classid[class_id], instance_id)] = (r, g, b)
+            
+    
+    return object2color, class_instance
+
+def do_mask_image(label, class_instance, object2color):
+    label_class = np.ndarray.astype(label/1000, np.int32)
+    label_instance = np.mod(label, 1000)
+    
+    h, w = label.shape
+    image_mask = np.zeros([h, w, 3], np.int32)
+    for class_id, instance_id in class_instance:
+        mask = np.zeros([h, w, 3], np.int32)
+        r, g, b = object2color[(id2classid[class_id], instance_id)]
+        
+        intersect = (label_class==class_id)*(label_instance==instance_id)
+        mask[:, :, 0] = r*intersect
+        mask[:, :, 1] = g*intersect
+        mask[:, :, 2] = b*intersect
+        
+        image_mask += mask
+        
+    return image_mask
+
+def create_color2category(class_instance, object2color):
+    category_ids = {}
+
+    for img_id, pair_list in class_instance.items():
+        category_ids[img_id] = {}
+        for class_id, instance_id in pair_list:
+            r, g, b = object2color[(id2classid[class_id], instance_id)]
+            category_ids[img_id][str((r, g, b))] = class_id
+    
+    return category_ids
+
+def create_sub_masks(mask_image):
+    width, height = mask_image.size
+
+    # Initialize a dictionary of sub-masks indexed by RGB colors
+    sub_masks = {}
+    for x in range(width):
+        for y in range(height):
+            # Get the RGB values of the pixel
+            pixel = mask_image.getpixel((x,y))[:3]
+
+            # If the pixel is not black...
+            if pixel != (0, 0, 0):
+                # Check to see if we've created a sub-mask...
+                pixel_str = str(pixel)
+                sub_mask = sub_masks.get(pixel_str)
+                if sub_mask is None:
+                    # Create a sub-mask (one bit per pixel) and add to the dictionary
+                    # Note: we add 1 pixel of padding in each direction
+                    # because the contours module doesn't handle cases
+                    # where pixels bleed to the edge of the image
+                    sub_masks[pixel_str] = Image.new('1', (width+2, height+2))
+                    
+                # Set the pixel value to 1 (default is 0), accounting for padding
+                sub_masks[pixel_str].putpixel((x+1, y+1), 1)
+
+    return sub_masks
+
+
+
+def create_sub_mask_annotation(sub_mask, image_id, category_id, annotation_id, is_crowd):
+    # Find contours (boundary lines) around each sub-mask
+    # Note: there could be multiple contours if the object
+    # is partially occluded. (E.g. an elephant behind a tree)
+    contours = measure.find_contours(sub_mask, 0.5, positive_orientation='low')
+
+    segmentations = []
+    polygons = []
+    for contour in contours:
+        # Flip from (row, col) representation to (x, y)
+        # and subtract the padding pixel
+        for i in range(len(contour)):
+            row, col = contour[i]
+            contour[i] = (col - 1, row - 1)
+
+        # Make a polygon and simplify it
+        poly = Polygon(contour)
+        poly = poly.simplify(1.0, preserve_topology=False)
+        polygons.append(poly)
+        try:
+            segmentation = np.array(poly.exterior.coords).ravel().tolist()
+        except:
+            continue
+        segmentations.append(segmentation)
+
+    # Combine the polygons to calculate the bounding box and area
+    multi_poly = MultiPolygon(polygons)
+    x, y, max_x, max_y = multi_poly.bounds
+    width = max_x - x
+    height = max_y - y
+    bbox = (x, y, width, height)
+    area = multi_poly.area
+
+    annotation = {
+        'segmentation': segmentations,
+        'iscrowd': is_crowd,
+        'image_id': image_id,
+        'category_id': category_id,
+        'id': annotation_id,
+        'bbox': bbox,
+        'area': area
+    }
+
+    return annotation
+
+def clean_image_list(image_list, annotations):
+    useful_id = set()
+    new_image_list = []
+    
+    for annotation in annotations:
+        useful_id.add(annotation['image_id'])
+    for image in image_list:
+        if image['id'] in useful_id:
+            new_image_list += [image]
+    return new_image_list
+    
